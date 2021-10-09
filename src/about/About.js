b815c9ac138975f2ed1430964e791650e197e50f
Jacob Hickman <strivelinkband@yahoo.com>
2021-10-08 21:22:19 -0500

    Fixed grammatical errors in Home.js and About.js, added gradient border to portrait on home page.

--- src/about/About.js
+++ src/about/About.js
@@ -11,33 +11,31 @@ export default function About() {
       <div className='about-portrait'>
         <img src={selfPortrait} alt='Self Portrait' className='card-img self-portrait' />
       </div>
-      <h3 className='section-title'>About Me</h3> 
+      <h3 className='section-title'>About Me</h3>
       <div>
         <article className='white-font'>
           <div className='about-me-text'>
-            <p>I've been passionate about the web since I was little.
-              While I was growing up, I would search the web for all types of things, from Rubik's
-              Cube tutorials, video game walkthroughs and information, and so much more. I've been
+            <p>I've been passionate about the web since I was young.
+              Growing up, I would search the web for all types of things, from Rubik's
+              Cube tutorials, video game walkthroughs, and much more. I've been
               living on the web for most of my life, and as time went by, the web inspired me to start programming.
             </p>
             <p>
-              I've been programming for a little over twelve years now, and I've spent most of that time programming in
-              Java.
-              I learned Java to be able to program games and mod a game called <a
+              I found my passion for programming at the age of twelve, when I started learning Java.
+              I studied Java in order to program my own games and mod a game called <a
                 href='https://www.minecraft.net/en-us/' style={{ textDecoration: 'none', color: 'lime' }}>Minecraft</a>.
-              I spent around six and a half years working with a many different teams of programmers, to help create
-              and configure Minecraft servers. I've always enjoyed working on a team, and am excited for all
+              I worked with many different teams of developers,
+              creating mods and configuration for a variety of Minecraft servers.
+              I've always enjoyed working on a team, and am excited for all
               the teams I'm going to meet in the future.
             </p>
             <p>Coding hasn't always been my passion though. In fact, I have learned many
               different skills that have nothing
-              to do with coding. Some of these skills include skateboarding, origami, speed cubing, machinic work,
-              plumbing,
-              and construction. I'm achieved the rank of Eagle Scout in the <a href='https://www.scouting.org/' style={{ textDecoration: 'none', color: 'gold' }}>Boy
-                Scounts of America</a>.
-              I've also learned a couple of different instruments, such as the acoustic and electric guitar, piano,
-              ukulele, ocarina,
-              and a couple more. I've always enjoyed learning new things, and will never stop.</p>
+              to do with coding. Such as skateboarding, origami, speed cubing, guitar,
+              and piano. 
+              I achieved the rank of Eagle Scout in the <a href='https://www.scouting.org/' style={{ textDecoration: 'none', color: 'gold' }}>Boy
+              Scounts of America</a> in 2016.
+              I've always enjoyed learning new things, and will never stop.</p>
           </div>
           <h5> Here's a list of services I can provide</h5>
           <div className='services'>
--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -8,19 +8,21 @@ export default function HomePage() {
     <div className='hp-bg'>
       <div className='hp'>
         <div className='card hp-card'>
-          <div className="portrait">
-            <img src={selfPortrait} alt='Self Portrait' className='card-img' />
+          <div className='portrait-border'>
+            <div className="portrait">
+              <img src={selfPortrait} alt='Self Portrait' className='card-img' />
+            </div>
           </div>
           <div className='card-body introduction'>
             <p style={{ color: 'white' }}>
-              Hi! My name is Jacob Hickman, I've been a professional programmer for 5 years, specializing in backend development.
+              Hi, my name is Jacob Hickman, I've been a full-stack web developer for five years, specializing in backend development.
               I graduated from <a href="https://thinkful.com/" className='intro-links' target='_blank' rel='noreferrer'>Thinkful</a> in May 2021,
-              where I learned the majority of my web development skills. I'm currently a freelance web developer residing
+              where I officially started my adventure into the tech world. I'm currently a freelance web developer residing
               in <a href='https://goo.gl/maps/tcZyJZZWkiJvMaC97' className='intro-links' target='_blank' rel='noreferrer'>San Antonio, Texas</a>.
             </p>
           </div>
         </div>
-      </div> 
+      </div>
     </div>
   )
 }
